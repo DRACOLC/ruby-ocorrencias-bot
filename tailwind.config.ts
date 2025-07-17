a8bce919751b01b419f166b6174c0b61e00be4a1
DRACOLC <lc8169926@gmail.com>
2025-07-17 19:51:31 -0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./client/**/*.{js,ts,jsx,tsx,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
